432e74bfc170d350233023022bfb78f0b89ede43
joncode21 <dwuw223@gmail.com>
2021-08-06 21:46:34 +0800

    ..

---
+++ src/CCACategory.java
@@ -0,0 +1,16 @@
+
+
+public class CCACategory 
+{
+	public String id;
+	public String category;
+	
+	
+	public CCACategory(String id, String category) 
+	   {
+		
+		this.id = id;
+		this.category = category;
+		}
+
+}
